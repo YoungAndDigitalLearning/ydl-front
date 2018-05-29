b4beff679f44a55ac905163168d13a8441023a6b
B4rtware <hansfrans47@hotmail.de>
2018-05-29 14:22:00 +0200

    started to integrate landingpage into main project

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,12 +1,51 @@
 <template>
-  <div class="djb-container">
-    <section class="djb-content">
-      <div class="djb-profile-button" v-on:click="toggleProfile"></div>
-      <content-section></content-section>
-    </section>
-    <section v-if="sprofile" transition="slide-toggle" class="djb-profile-content">
-      <profile-sidebar></profile-sidebar>
-    </section>
+  <div>
+    <div class="header-dark">
+      <nav class="navbar navbar-dark navbar-expand-md navigation-clean-search">
+        <div class="container">
+          <a class="navbar-brand" href="#">Y&D Learning</a>
+          <button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="nav navbar-nav">
+              <li class="nav-item" role="presentation">
+                <a class="nav-link" href="#">Home</a>
+              </li>
+              <li class="dropdown">
+                <a class="dropdown-toggle nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">Erkunden </a>
+                <div class="dropdown-menu" role="menu">
+                  <a class="dropdown-item" role="presentation" href="#">First Item</a>
+                  <a class="dropdown-item" role="presentation" href="#">Second Item</a>
+                  <a class="dropdown-item" role="presentation" href="#">Third Item</a>
+                </div>
+              </li>
+            </ul>
+            <form class="form-inline mr-auto" target="_self">
+              <div class="form-group">
+                <label for="search-field">
+                  <i class="fa fa-search"></i>
+                </label><input class="form-control search-field" type="search" name="search" id="search-field"></div>
+            </form>
+            <span class="navbar-text">
+              <a href="#" class="login" @click="showModal = true" data-toggle="modal" data-target="#login_modal">Log In</a>
+            </span>
+            <a class="btn btn-light action-button" role="button" href="#">Sign Up</a>
+          </div>
+        </div>
+      </nav>
+      <div class="container hero">
+        <div class="row">
+          <div class="col-md-8 offset-md-2">
+            <h1 class="text-center">Young&Digital Learning</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="login_screen">
+      <ydl-login v-if="showModal" @close="showModal = false"></ydl-login>
+    </div>
   </div>
 </template>
 
@@ -14,60 +53,25 @@
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
-import ProfileSidebar from "@/components/ProfileSidebar"
-import ContentSection from "@/components/ContentSection"
+import Login from "@/components/Login"
+// require("./../assets/css/Article-Clean.css")
 
 export default {
   name: "MainPage",
   data () {
     return {
-      "sprofile": true
-    }
-  },
-  methods: {
-    toggleProfile () {
-      this.sprofile = !this.sprofile
+      showModal: false
     }
   },
   components: {
-    "profile-sidebar": ProfileSidebar,
-    "content-section": ContentSection
+    "ydl-login": Login
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.djb-container {
-  display: flex;
-  align-items: stretch;
-  flex-wrap: wrap;
-  height: 100%;
-}
-
-.djb-content {
-  background-color: aqua;
-  flex: 1;
-  height: 100%;
-  padding: 5px;
-}
-
-.djb-profile-content {
-  background-color: deeppink;
-  width: 350px;
-  padding: 5px;
-}
-
-.djb-profile-button {
-  background-color: chartreuse;
-  float: right;
-  margin: 5px;
-  height: 32px;
-  width: 32px;
-}
-
-.slide-toggle {
-  &-enter {
-    transform: translateX(350px);
-  }
-}
+@import "./../assets/css/Article-Clean.css";
+@import "./../assets/css/Header-Dark.css";
+@import "./../assets/css/Login-Form-Dark.css";
+@import "./../assets/css/styles.css";
 </style>
