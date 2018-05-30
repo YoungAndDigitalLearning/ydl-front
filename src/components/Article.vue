2fc065413e5a5c1f1acaf9dbb23fc27ba3e8196a
jan-hendrik <jan-hendrik.fabricius@t-online.de>
2018-05-30 16:46:35 +0200

    little changes on article component

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,19 +1,19 @@
-props:['article_entry']
 <template>
   <div id="landing_article" class="article-clean">
+    <p> {{ articleEntry }} </p>
     <div class="container">
       <div class="row">
         <div class="col-lg-10 col-xl-8 offset-lg-1 offset-xl-2">
           <div class="intro">
-            <h1 class="text-center"> {{ article_entry.headline }} </h1>
+            <h1 class="text-center"> {{ articleEntry.headline }} </h1>
               <p class="text-center">
                   <span class="by">by</span>
-                  <a href="#">{{article_entry.author}}</a>
+                  <a href="#">{{articleEntry.author}}</a>
                   <span class="date">Sept 8th, 2016 </span>
               </p>
-              <img v-if="article_entry.image != ''" class="img-fluid" src="assets/img/desk.jpg">
+              <img v-if="articleEntry.image != ''" class="img-fluid" src="assets/img/desk.jpg">
             </div>
-          <div v-html="article_entry.text" class="text"></div>
+          <div v-html="articleEntry.text" class="text"></div>
         </div>
       </div>
     </div>
@@ -25,14 +25,15 @@ export default {
   name: "Article",
   data () {
     return {
-      article_entry: {}
+      articleEntry: {}
     }
   },
   created: function () {
-    fetch("../assets/json/article.json").then(response => response.json()).then(json => {
-      this.article_entry = json.article_entry
-    }
-    )
+    console.log("Hello World")
+    this.$http.get("http://localhost/article.json")
+      .then(function (response) {
+        console.log(response.data)
+      })
   }
 }
 </script>
