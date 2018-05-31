419459558ab69f9ecda650eee284ed5c6fd41f7a
jan-hendrik <jan-hendrik.fabricius@t-online.de>
2018-05-31 17:06:41 +0200

    beautified the Profile page added Sidebar and course overview

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,31 +2,31 @@
   <transition name="modal">
     <div class="modal-mask">
       <div class="modal-wrapper">
-    <div id="login_modal" class="fade show" tabindex="-1" role="dialog" style="margin:auto;">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="login-dark" style="height:550px;">
-            <button class="close" type="button" data-dismiss="modal" @click="$emit('close')" aria-label="Close" style="color:rgb(255,255,255);margin:0 5 0 0;">
-              <span class="close_modal" aria-hidden="true">×</span>
-            </button>
-            <form method="post">
-              <h2 class="sr-only">Login Form</h2>
-              <div class="illustration">
-                <i class="icon ion-ios-locked-outline"></i>
+        <div id="login_modal" class="fade show" tabindex="-1" role="dialog" style="margin:auto;">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="login-dark" style="height:550px;">
+                <button class="close" type="button" data-dismiss="modal" @click="$emit('close')" aria-label="Close" style="color:rgb(255,255,255);margin:0 5 0 0;">
+                  <span class="close_modal" aria-hidden="true">×</span>
+                </button>
+                <form method="post">
+                  <h2 class="sr-only">Login Form</h2>
+                  <div class="illustration">
+                    <i class="icon ion-ios-locked-outline"></i>
+                  </div>
+                  <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email"></div>
+                  <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
+                  <div class="form-group">
+                    <button class="btn btn-primary btn-block" type="submit">Log In</button>
+                    <button class="btn btn-primary btn-block">Konto erstellen</button>
+                  </div>
+                  <a href="#" class="forgot">Passwort vergessen</a>
+                </form>
               </div>
-              <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email"></div>
-              <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
-              <div class="form-group">
-                <button class="btn btn-primary btn-block" type="submit">Log In</button>
-                <button class="btn btn-primary btn-block">Konto erstellen</button>
-              </div>
-              <a href="#" class="forgot">Passwort vergessen</a>
-            </form>
+            </div>
           </div>
         </div>
       </div>
-      </div>
-    </div>
     </div>
   </transition>
 </template>
