c5709bae9b440f971efe31f4ef7a0b4ad11efa58
B4rtware <hansfrans47@hotmail.de>
2018-06-01 02:07:53 +0200

    added course creation (very simple)

---
+++ src/components/CoursePage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card">
+    <div class="card-header"><h1>Deutsch</h1>A1.1</div>
+    <div class="card-body">
+      <h2 class="card-title">Neuigkeiten</h2>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum eu mauris ut lacinia. Sed convallis leo posuere, placerat neque a, blandit urna. Sed id ultricies felis. Ut viverra ut odio id elementum. Sed sodales risus enim, at viverra purus tempor id. Proin at accumsan lectus. Etiam lacinia tellus risus, a ultricies nulla fringilla at. Quisque a ex quis libero scelerisque vulputate. Morbi dui sapien, consequat eget molestie ac, pellentesque ut tortor. Nunc et dictum tellus. Praesent urna ante, molestie tristique felis et, feugiat euismod elit. Phasellus vehicula felis felis, eget laoreet libero tincidunt sit amet. Pellentesque posuere semper erat eu dapibus. Maecenas id ligula turpis.
+
+Nulla facilisi. Pellentesque non posuere neque, sit amet blandit sem. Proin ut dolor convallis, mollis urna nec, varius ipsum. Phasellus rhoncus sit amet libero non maximus. Cras et vehicula odio. Vestibulum sodales erat eu nibh euismod, at porttitor tellus auctor. In id efficitur nisl. Proin rhoncus mi sit amet leo ultrices semper. Pellentesque euismod, quam eu semper facilisis, nisl odio pellentesque massa, sed congue felis nulla eget libero. Quisque malesuada felis vitae felis ornare, quis fermentum quam imperdiet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam a pretium metus. Vestibulum ante odio, sagittis et urna mattis, auctor bibendum dui. Proin facilisis nunc eu purus maximus luctus. Quisque sit amet justo odio. Sed ut sollicitudin enim.
+      <div class="courseweek-container">
+      <ydl-courseweek></ydl-courseweek>
+      <ydl-courseweek></ydl-courseweek>
+      <ydl-courseweek></ydl-courseweek>
+      <ydl-courseweek></ydl-courseweek>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CourseWeek from "@/components/CourseWeek"
+
+export default {
+  name: "courseview",
+  components: {
+    "ydl-courseweek": CourseWeek
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.card {
+  color:#56585b;
+  margin-top: 25px;
+  border-radius: 0;
+  font-family:'Lora', serif;
+  background-color: rgba(200, 200, 200, 0.95);
+  font-size:14px;
+}
+
+.card-text > p{
+  color:#000000;
+}
+
+.courseweek-container {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/CreateCourse.vue
@@ -0,0 +1,131 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="sign-up-dark">
+              <div class="form-container">
+              <div class="close-button" type="button" @click="$emit('close')" aria-label="Close">
+                  <i class="icon ion-close-circled"></i>
+              </div>
+              <div class="illustration">
+                <i class="icon ion-android-create"></i>
+              </div>
+              <form method="post">
+                <h2 class="sr-only">Login Form</h2>
+                <div class="form-group"><input class="form-control" type="text" name="name" placeholder="name"></div>
+                <div class="form-group"><input class="form-control" type="text" name="text" placeholder="deadline"></div>
+                <div class="form-group"><input class="form-control" type="text" name="student_count" placeholder="number of participants"></div>
+                <div class="form-group">
+                  <button class="btn btn-primary btn-block" type="submit">Create Course</button>
+                </div>
+              </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "createcourse"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/fonts/ionicons.min.css";
+
+/* Signup */
+
+.sign-up-dark {
+  background-image: url(../assets/img/star-sky.jpg);
+  background-size:cover;
+  width: 600px;
+
+  form {
+    .btn-primary {
+      background-color: #214a80;
+      border: none;
+      padding: 11px;
+    }
+  }
+
+  /* contains the icons and the form */
+  .form-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color:#1e2833;
+    padding: 40px;
+    margin: 40px 70px;
+  }
+
+  .form-control {
+    background: none;
+    border: none;
+    border-bottom: 1px solid #434a52;
+    border-radius: 0;
+    outline: none;
+    box-shadow: none;
+    color: inherit;
+
+    &:focus {
+      border-color: #2980ef;
+    }
+  }
+
+  .close-button {
+    flex: 1;
+    align-self: flex-end;
+    font-size: 32px;
+
+    &:hover {
+      color: #e03131
+    }
+  }
+
+  .illustration {
+    text-align: center;
+    padding: 15px 0 20px;
+    font-size: 100px;
+    color: #2980ef;
+  }
+}
+
+/* Signup Transition */
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
