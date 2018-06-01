176e2ac94d0d07773024733989df5f0dfb97d5b0
jan-hendrik <jan-hendrik.fabricius@t-online.de>
2018-06-01 08:56:27 +0200

    separated some Components for reusability and little css customizations

---
+++ src/components/Calendar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="calendar">
+    <ul class="list-group">
+      <a v-for="task in tasks" :key="task.key" :task="taks" href="#" class="list-group-item list-group-item-action heading">
+        {{ task.title }}
+      </a>
+    </ul>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calendar",
+  props: {
+    tasks: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.calendar {
+  color: black;
+}
+
+.calendar > ul > li{
+  text-decoration: none;
+}
+
+.heading {
+  font-size: 15px;
+  color: black;
+}
+
+span {
+  font-size: 15px;
+}
+</style>
