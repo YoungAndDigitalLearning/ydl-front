3118a6453a1d6b27265387028566e67dfe4934d7
B4rtware <hansfrans47@hotmail.de>
2018-06-06 18:07:13 +0200

    password field | server is now on 0.0.0.0 for mobile access

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -7,9 +7,9 @@
       <form @submit.prevent="authenticate">
         <h2 class="sr-only">Login Form</h2>
         <div class="form-group"><input class="form-control" type="text" name="username" placeholder="Benutzername"></div>
-        <div class="form-group"><input class="form-control" type="text" name="password" placeholder="Passwort"></div>
+        <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Passwort"></div>
         <div class="form-group">
-          <button class="btn btn-primary btn-block" type="submit">Logs In</button>
+          <button class="btn btn-primary btn-block" type="submit">Log In</button>
           <a class="btn btn-primary btn-block" href="/#/signup">Konto erstellen</a>
         </div>
         <a href="#" class="forgot">Passwort vergessen</a>
