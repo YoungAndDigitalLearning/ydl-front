48ba568dc073c7f12e2c55e3edaa4b7e3f30a3a6
jan-hendrik <jan-hendrik.fabricius@t-online.de>
2018-06-06 10:54:07 +0200

    implemented that detailed course view is loaded, when a link is clicked, look trello for more details

--- src/components/Course.vue
+++ src/components/Course.vue
@@ -1,16 +1,20 @@
 <template>
-    <a class="list-group-item list-group-item-action" href="www.google.de">
+<div>
+  <ul class="list-group" v-for="course in courses" v-bind:key="course.id">
+    <a class="list-group-item list-group-item-action" v-bind:href="'/profile/' + course.id">
       <h4> {{course.title}} </h4>
       <div v-html="course.details"></div>
     </a>
+  </ul>
+</div>
 </template>
 
 <script>
 export default {
   name: "Course",
   props: {
-    course: {
-      type: Object,
+    courses: {
+      type: Array,
       required: true
     }
   }
