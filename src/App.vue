1f6353f27ca9228f4be707166ff06bd634024b2a
B4rtware <hansfrans47@hotmail.de>
2018-06-09 03:16:37 +0200

    modified theme | added global sass script | cleaned up some components

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,17 @@
 <template>
   <div id="app">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fir=yes">
-    <div class="header-dark">
-      <nav class="navbar navbar-dark navbar-expand-md navigation-clean-search">
-        <div class="container">
-          <a class="navbar-brand" href="#">Y&D Learning</a>
-          <button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navcol-1">
-            <ul class="nav navbar-nav">
-              <li class="nav-item" role="presentation">
-                <a class="nav-link" href="/#/">Home</a>
-              </li>
-            </ul>
-            <form class="form-inline mr-auto" target="_self">
-              <div class="form-group">
-                <label for="search-field">
-                  <i class="fa fa-search"></i>
-                </label><input class="form-control search-field" type="search" name="search" id="search-field"></div>
-            </form>
-            <div v-if="!loggedIn">
-              <span class="navbar-text">
-                <a href="/#/login" class="login" @toggleLoggedIn="toggleLogin">Log In</a>
-              </span>
-              <a class="btn btn-light action-button" role="button" href="/#/signup">Sign Up</a>
-            </div>
-            <div v-else>
-              <a class="btn btn-light action-button" role="button" @click="logout">Log Out</a>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </div>
-    <div class="ydl-content">
+    <ydl-navbar/>
+    <section class="ydl-content">
       <router-view/>
-    </div>
+    </section>
   </div>
 </template>
 
 <script>
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
+
+import Navbar from "@/components/Navbar.vue"
 
 export default {
   name: "App",
@@ -65,16 +34,13 @@ export default {
       this.loggedIn = this.$session.exists()
       console.log(this.loggedIn)
     }
-  }
+  },
+  components: {"ydl-navbar": Navbar}
 }
 </script>
 
 <style lang="scss">
-@import "./assets/css/Header-Dark.css";
-@import "./assets/css/styles.css";
-
-// import all bootstrap sass files
-@import "node_modules/bootstrap/scss/bootstrap";
+@import "styles/global";
 
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
@@ -85,35 +51,21 @@ export default {
   height: 100%;
 }
 
-.header-dark {
-  position: fixed;
-  width: 100%;
-  padding: 0px;
-  z-index: 999;
-  background-color: rgba(6, 137, 179, 1);
+.ydl-header {
+  background-color: $primary;
+  height: $ydl-header-height;
 }
 
 .ydl-content {
-  padding-top: 90px;
-  padding-bottom: 90px;
-  z-index: 0;
-  overflow:auto;
+  overflow-y: auto;
   height: 100%;
+}
 
-  @include media-breakpoint-down(sm)
-  {
-    // deactivate the padding
-    padding: 0px;
-  }
+body {
+  padding-top: $ydl-header-height;
 }
 
 html {
     height: 100%;
-    // background:url(./assets/img/mountain_bg.jpg);
-    background-color: white;
-    background-repeat: no-repeat;
-    background-position: center top;
-    background-size: cover;
-    background-attachment: fixed;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-primary bg-secondary fixed-top">
+    <a class="navbar-brand" href="#">Y&D Learning</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home
+            <span class="sr-only">(current)</span>
+          </a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/#/signup"><fa-icon icon="edit" /> Sign In</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/#/login"><fa-icon icon="unlock" /> Login</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "navbar"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "styles/global";
+
+.navbar {
+  height: $ydl-header-height;
+}
+</style>
