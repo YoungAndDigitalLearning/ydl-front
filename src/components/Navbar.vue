f30127adfbcf7de9d033d0af7b2e6b76961c930a
B4rtware <34386047+B4rtware@users.noreply.github.com>
2018-06-11 12:23:32 +0200

    fixed navbar toggler icon | added login form validation | modified login style

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-primary bg-secondary fixed-top">
     <a class="navbar-brand" href="#">Y&D Learning</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <fa-icon icon="bars"/>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav mr-auto">
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue"
 import Router from "vue-router"
 import BootstrapVue from "bootstrap-vue"
+import VueSession from "vue-session"
+import VeeValidate from "vee-validate"
 
 import LandingPage from "@/components/LandingPage"
 import ProfilePage from "@/components/ProfilePage"
@@ -9,14 +11,13 @@ import Sandbox from "@/components/Sandbox"
 import LoginPage from "@/components/LoginPage"
 import SignupPage from "@/components/SignupPage"
 
-import VueSession from "vue-session"
-
 import fontawesome from "@fortawesome/fontawesome"
 import freeSolid from "@fortawesome/fontawesome-free-solid/"
 import FontAwesomeIcon from "@fortawesome/vue-fontawesome"
 
 fontawesome.library.add(freeSolid)
 
+Vue.use(VeeValidate)
 Vue.use(VueSession)
 Vue.use(Router)
 Vue.use(BootstrapVue)
