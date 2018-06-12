07cca4438a113ab9c3ce60ebea030159ee7837c1
B4rtware <34386047+B4rtware@users.noreply.github.com>
2018-06-12 16:45:43 +0200

    added form label animation | updated sign in style | added sign in validators

---
+++ src/components/FormLabel.vue
@@ -0,0 +1,36 @@
+<template>
+    <transition name="move" appear>
+        <label :for="inputID"><slot></slot></label>
+    </transition>
+</template>
+
+<script>
+export default {
+  name: "FormLabel",
+  props: {
+    inputID: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
+<style lang="scss" scoped>
+label {
+  position: absolute;
+  color: #444444;
+  // manual fit to the placeholder position
+  left: 12px;
+  top: 6px;
+
+  // enable transition for top
+  transition: all 0.5s;
+  // disable hover events etc
+  pointer-events: none;
+}
+</style>
--- src/components/SignupPage.vue
+++ src/components/SignupPage.vue
@@ -1,16 +1,39 @@
 <template>
   <div class="sign-up-container">
     <div class="form-container">
+      <div class="ydl-alert-box">
+      <b-alert :show="dismissCountDown"
+              dismissible
+              variant="danger"
+              @dismissed="dismissCountDown=0"
+              @dismiss-count-down="countDownChanged">
+        <p v-for="(error, index) in nonFieldErrors" :key="index">{{ error }}</p>
+      </b-alert>
+      </div>
       <div class="illustration">
         <i class="icon ion-android-create"></i>
       </div>
       <form @submit.prevent="handleSubmit">
         <h2 class="sr-only">Login Form</h2>
         <div class="form-group">
-          <input class="form-control" type="text" name="username" placeholder="Username" required v-model="user.username">
+          <input class="form-control" v-validate="'required|alpha_dash'" id="username" type="text" name="username" v-model="form.username" required>
+          <ydl-label inputID="username">Username</ydl-label>
+          <span v-show="errors.has('username')" class="required">{{ errors.first("username") }}</span>
         </div>
         <div class="form-group">
-          <input class="form-control" type="email" name="email" placeholder="Email">
+          <input class="form-control" v-validate="'required'" id="password1" type="password" name="password1" v-model="form.password" required>
+          <ydl-label inputID="password1">Password</ydl-label>
+          <span v-show="errors.has('password1')" class="required">{{ errors.first("password1") }}</span>
+        </div>
+        <div class="form-group">
+          <input class="form-control" v-validate="{required: true, is: form.password}" id="password2" type="password" name="password2" required>
+          <ydl-label inputID="password2">Confirm Password</ydl-label>
+          <span v-show="errors.has('password2')" class="required">{{ errors.first("password2") }}</span>
+        </div>
+        <div class="form-group">
+          <input class="form-control" v-validate="'required|email'" id="email" type="email" name="email" v-model="form.email" required>
+          <ydl-label inputID="email">Email</ydl-label>
+          <span v-show="errors.has('email')" class="required">{{ errors.first("email") }}</span>
         </div>
         <div class="form-group">
           <button class="btn btn-primary btn-block" type="submit">Sign Up</button>
@@ -21,29 +44,57 @@
 </template>
 
 <script>
+import FormLabel from "@/components/FormLabel"
+
 import axios from "axios"
 
 export default {
   name: "SignupPage",
   data () {
     return {
-      user: {
+      dismissSecs: 4,
+      dismissCountDown: 0,
+      showDismissibleAlert: false,
+      nonFieldErrors: [],
+      form: {
         username: "",
-        password: ""
-      },
-      response: {}
+        password: "",
+        email: ""
+      }
     }
   },
   methods: {
+    validateBeforeSubmit () {
+      this.$validator.validateAll()
+        .then((result) => {
+          if (result) {
+            this.handleSubmit()
+          }
+        })
+    },
+    countDownChanged (dismissCountDown) {
+      this.dismissCountDown = dismissCountDown
+    },
+    showAlert () {
+      this.dismissCountDown = this.dismissSecs
+    },
     handleSubmit () {
-      axios.post("http://35.185.239.7:2222/api/users/", this.user).then(Response => {
-        console.log(Response.status)
-        if (Response.status === 201) {
-          this.$router.push("/profile")
-        }
-      })
+      axios.post("http://35.185.239.7:2222/api/users/", this.form)
+        .then(response => {
+          console.log(response.status)
+          if (response.status === 201) {
+            this.$router.push("/profile")
+          }
+        })
+        .catch(error => {
+          if (error.response.status === 400) {
+            this.nonFieldErrors = error.response.data.non_field_errors
+            this.showAlert()
+          }
+        })
     }
-  }
+  },
+  components: {"ydl-label": FormLabel}
 }
 </script>
 
@@ -51,6 +102,26 @@ export default {
 @import "../assets/fonts/ionicons.min.css";
 
 @import "styles/global";
+
+/* Alert Box */
+.ydl-alert-box {
+  position: fixed;
+  top: 200px;
+}
+
+/* Form Group */
+// make form group position relative so an absolute child will be within this element
+.form-group {
+  position: relative;
+}
+
+// for the animation if input is selected
+.form-control {
+  &:focus + label, &:valid + label {
+    transform: translateY(-100%);
+    font-size: 75%;
+  }
+}
 
 /* Signup */
 .sign-up-container {
