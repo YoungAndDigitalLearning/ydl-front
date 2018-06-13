286d87129c90789710db86b9a693ecdc51a10ba5
B4rtware <hansfrans47@hotmail.de>
2018-06-13 11:28:32 +0200

    removed https

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -87,7 +87,8 @@ export default {
       this.dismissCountDown = this.dismissSecs
     },
     authenticate () {
-      axios.post("https://35.185.239.7:2222/api/token-auth/", this.form)
+      // axios.post("http://35.185.239.7:2222/api/token-auth/", this.form)
+      this.$api.post("token-auth", this.form)
         .then(response => {
           console.log(response)
           if (response.status === 200 && "token" in response.data) {
