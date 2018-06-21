614a9d1172715a13a456ece7422768a6679640bf
B4rtware <34386047+B4rtware@users.noreply.github.com>
2018-06-22 00:38:01 +0200

    forgot to add

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -74,7 +74,7 @@ export default {
       console.log("authenticate user")
       this.$store.dispatch("login", this.form)
         .then(() => {
-          this.$router.push("/#/profile/" + this.user.id)
+          this.$router.push("profile/" + this.user.id)
         })
     }
   }
